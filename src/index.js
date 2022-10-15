3b3a2ed88d9d2807a6d20b583446e65f91efafb4
Jawdat Abadullah <Jawdat.89@outlook.com>
2022-10-16 02:21:37 +0300

    game funtionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
-import { Player } from './entities/player.entity.js';
-const init = document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded');
-    const p1 = new Player('misho', 'X');
-    const p2 = new Player('mishA', 'O');
+import { isAllButtonsOccupied, isThereAWinner, resetButtons } from "./helpers/buttons.js";
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOMContentLoaded");
+    const players = [
+        {
+            name: "misho",
+            sign: "X"
+        },
+        {
+            name: "mishA",
+            sign: "O"
+        }
+    ];
+    let turn = Math.floor(Math.random() * 10) > 5 ? players[0].name : players[1].name;
+    const buttons = document.querySelectorAll('.section-button');
+    document.querySelector('.header-player_turn').innerHTML = `Turn for: ${turn}`;
+    document.querySelector('.section-button_reset').addEventListener('click', () => {
+        resetButtons(buttons);
+    });
+    resetButtons(buttons);
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            if (button.innerHTML === '&nbsp;') {
+                button.innerHTML = players.find(p => p.name === turn).sign;
+                turn = turn === players[0].name ? players[1].name : players[0].name;
+                checkGameState(buttons, players);
+            }
+        });
+    });
 });
-export default init;
+const checkGameState = (buttons, players) => {
+    const isDraw = isAllButtonsOccupied(buttons);
+    if (isDraw) {
+        document.querySelector('.section-game_result').innerHTML = 'DRAW';
+        return;
+    }
+    const winner = isThereAWinner(buttons, players);
+    if (winner) {
+        document.querySelector('.section-game_result').innerHTML = `The Winner is ${winner}`;
+        return;
+    }
+};
---
+++ src/types/game-button.type.ts
@@ -0,0 +1,1 @@
+export type GameButton = {btnIdx: string, btnVal: string};
