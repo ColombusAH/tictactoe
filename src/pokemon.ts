b7d3ba88fdc16c8401439be229e22570a7b1336b
Avner <avner.h@nanox.vision>
2022-10-21 00:12:40 +0300

    pokemon

---
+++ src/pokemon.ts
@@ -0,0 +1,26 @@
+export abstract class Pokemon {
+    private _live: number;
+
+    public get live(): number {
+        return this._live;
+    }
+
+    private _attack: number;
+
+    public get attack(): number {
+        return this._attack;
+    }
+
+    constructor(live: number, attack: number) {
+        if (!live) {
+            throw new Error('live must grater than zero');
+        }
+
+        if (!attack) {
+            throw new Error('attack must grater than zero');
+        }
+
+        this._live = live;
+        this._attack = attack;
+    }
+}
